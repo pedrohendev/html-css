ffb7a41ce067b953acabafd52b66cfeee753c513
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-07-12 23:13:26 -0300

    novo

--- projeto/Dunamis eu/src/javascript/script.js
+++ projeto/Dunamis eu/src/javascript/script.js
@@ -30,3 +30,69 @@ const shadowHeader = () => {
     : header.classList.remove("shadow-header");
 };
 window.addEventListener("scroll", shadowHeader);
+
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_2k3jiay','template_rxbbf6o','#contact-form','0hSLbow_Naj77Cu2a')
+    .then(() =>{
+        contactMessage.textContent = 'Message send successfully ✅'
+
+        setTimeout(()=>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+        
+    }, ()=> {
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+const scrollUp = () =>{
+  const scrollUp = document.getElementById('scroll-up')
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                      : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+            if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionsClass.classList.add('active-link')
+            }else{
+                sectionsClass.classList.remove('active-link')
+            }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+const sr = ScrollReveal({
+  origin: `top`,
+  distance: `60px`,
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`.home__perfil, .about__image .contact__mail`, {origin: `right`})
+
+sr.reveal(`.home__name, .home__info,
+           .about__container .section__title-1, .about__info,
+           .contact__social, .contact__data`, {origin: `left`})
+           
+sr.reveal(`.services__card .projects__card`, {interval: 100})
